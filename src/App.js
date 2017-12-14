f52e55b3f36bc9a89914e5d497a2069caeb5e870
tpyoung <tylerparkeyoung@gmail.com>
2017-12-14 13:18:57 -0800

    adds get method for posts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
+import firebase from './configs/FirebaseConfig.js'
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
       currentItem: '',
-      username: ''
+      username: '',
+      items: []
     }
   }
-
+  componentDidMount(){
+    const itemsRef = firebase.database().ref('items');
+    itemsRef.on('value', (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          title: items[item].title,
+          user: items[item].user
+        })
+      }
+      this.setState({
+        items: newState
+      })
+    })
+  }
 
   handleChange = (e) => {
-    console.log(e.target.name);
     this.setState({
       [e.target.name]: e.target.value
     });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+      const itemsRef = firebase.database().ref('items');
+      const item = {
+        title: this.state.currentItem,
+        user: this.state.username
+      }
+      itemsRef.push(item);
+      this.setState({
+        currentItem: '',
+        username: ''
+      });
   }
   
   render() {
@@ -28,15 +59,23 @@ class App extends Component {
         </header>
         <div className='container'>
           <section className='add-item'>
-              <form>
+              <form onSubmit={this.handleSubmit}>
                 <input type="text" name="username" placeholder="What's your name?" onChange= {this.handleChange} value = {this.state.username} />
                 <input type="text" name="currentItem" placeholder="What are you bringing?" onChange= {this.handleChange} value = {this.state.currentItem} />
                 <button>Add Item</button>
               </form>
           </section>
           <section className='display-item'>
-            <div className='wrapper'>
+            <div className="wrapper">
               <ul>
+                {this.state.items.map((item) => {
+                  return (
+                    <li key={item.id}>
+                      <h3>{item.title}</h3>
+                      <p>brought by: {item.user}</p>
+                    </li>
+                  )
+                })}
               </ul>
             </div>
           </section>
